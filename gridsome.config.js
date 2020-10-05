9a7c846feb32855b26cbe310405f84d4115c2f83
David <milosevicdavid@gmail.com>
2020-10-05 16:37:03 +0200

    template za blog

---
+++ gridsome.config.js
@@ -0,0 +1,44 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const tailwindcss = require("tailwindcss")
+
+
+module.exports = {
+  siteName: 'Davidian',
+
+  templates: { 
+    ContentfulPortfolioBlog: [
+      {
+path: '/blog/:slug',
+        component: './src/templates/ContentfulPorfolioBlog.vue'
+      
+      }
+      
+    ]
+    
+  },
+  plugins: [
+    {
+    use: "@gridsome/source-contentful",
+      options: {
+      space: process.env.CONTENTFUL_SPACE,
+      accessToken: process.env.CONTENTFUL_TOKEN,
+      host: "cdn.contentful.com",
+      typeName: "Contentful"
+    },
+}
+],
+ css: {
+  loaderOptions: {
+    postcss: {
+      plugins: [
+        tailwindcss
+      ],
+    },
+  },
+ }
+}
